8c1de66845336575feb2d89741075db483d5c534
BubblePenguin <70269869+BubblePenguin@users.noreply.github.com>
2023-03-26 20:20:45 +0000

    Deploying to gh-pages from @ BubblePenguin/goit-js-hw-09@ad7254cc1f94da7af2bd7bc75be099fcee870f0b 🚀

---
+++ 01-color-switcher.13116a37.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;e.disabled=!0;var d=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))};t.addEventListener("click",(function(){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.13116a37.js.map
